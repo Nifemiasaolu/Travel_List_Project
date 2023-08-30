5a602981bd1b1bdfb5447d615f5e3ca9a40cb5a3
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-08-30 22:24:50 +0100

    App component

---
+++ src/Components/App.js
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import Logo from "./Logo.js";
+import Form from "./Form.js";
+import PackingList from "./PackingList.js";
+import Stats from "./Stats.js";
+
+export default function App() {
+  const [items, setItems] = useState([]);
+
+  // Add new items to empty array
+  function handleAddedItems(item) {
+    setItems((items) => [...items, item]);
+  }
+
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map(
+        (item) => (item.id === id ? { ...item, packed: !item.packed } : item) // This is how to update and object in an array.
+      )
+    );
+  }
+
+  function handleClearList(){
+    const confirmed = window.confirm("Are you sure you want to delete all items?");
+
+    if (confirmed) setItems([]);
+  }
+  return (
+    <div className="app">
+      <Logo />
+      <Form onAddItems={handleAddedItems} />
+      <PackingList
+        items={items}
+        onDeleteItem={handleDeleteItem}
+        onToggleItem={handleToggleItem}
+        onClearList={handleClearList}
+      />
+      <Stats items={items} />
+    </div>
+  );
+}
+
+
+
+
+
